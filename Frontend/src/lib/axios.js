3a269e61d803a15f4008d0277c827849d4395451
Diganta <digantachakraborty688@gmail.com>
2025-08-13 10:10:39 +0530

    changing env files

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // ✅ Works in Node
   withCredentials: true,
 });
-
